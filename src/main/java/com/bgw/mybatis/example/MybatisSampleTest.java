b83200566a69556f0dc20ef5588291b44636e914
bingowang <bingowangzhb@gmail.com>
2021-01-09 08:45:57 +0800

    update

--- src/main/java/com/bgw/mybatis/example/MybatisSampleTest.java
+++ src/main/java/com/bgw/mybatis/example/MybatisSampleTest.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.io.VFS;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.util.List;
@@ -20,37 +22,45 @@ import java.util.List;
  **/
 public class MybatisSampleTest {
 
+    static Logger log = LoggerFactory.getLogger(MybatisSampleTest.class);
 
     public static void main(String[] args) throws Exception {
 
 
         String resource = "conf/mybatis-config.xml";
-
         InputStream inputStream = Resources.getResourceAsStream(resource);
-
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        // VFS vfs = new DefaultVFS();
-        // vfs.list(".").forEach(System.out::println);
 
         selectByMapper(sqlSessionFactory);
     }
 
+    public static void testVfs() throws Exception {
+        VFS vfs = new DefaultVFS();
+        vfs.list(".").forEach(log::info);
+    }
+
+
     public static void selectSqlSession(SqlSessionFactory sqlSessionFactory) {
         try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
             Blog blog = sqlSession.selectOne("com.bgw.mybatis.example.mapper.BlogMapper.getBlog", 1);
-            System.out.println("blog = " + blog);
+            log.info("blog : {}", blog);
         }
     }
 
     public static void selectByMapper(SqlSessionFactory sqlSessionFactory) {
         try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
             BlogMapper blogMapper = sqlSession.getMapper(BlogMapper.class);
+            log.info("bolgMapper.class {}, hashCode: {}", blogMapper.getClass(), blogMapper.hashCode());
             Blog blog = blogMapper.getBlog(1);
-            System.out.println("blog = " + blog);
+            log.info("blog : {}", blog);
+
+
+            BlogMapper blogMapper2 = sqlSession.getMapper(BlogMapper.class);
+            log.info("bolgMapper2.class {}, hashCode: {}", blogMapper2.getClass(), blogMapper2.hashCode());
 
             List<Blog> blogs = blogMapper.getBlogs();
             for (Blog b : blogs) {
-                System.out.println("b = " + b);
+                log.info("b : {}", b);
             }
         }
     }
